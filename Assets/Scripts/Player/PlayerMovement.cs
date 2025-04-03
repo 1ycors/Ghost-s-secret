1e8495d9d2262fe0204314a492efb9f94a388125
lycoris <kazenova_altyn@mail.com>
2025-04-03 22:59:12 +0500

    инициализация репозитория

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float runningSpeed = 20f;
+    private int lockCount = 0;
+    public bool isLocked => lockCount > 0;
+
+    private Vector2 movement;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Movement();
+    }
+    private void FixedUpdate()
+    {
+        if (isLocked)
+        {
+            rb.velocity = Vector2.zero;  // Останавливаем движение, если заблокировано
+            return;
+        }
+
+        rb.velocity = movement * runningSpeed;
+    }
+    private void Movement()
+    {
+        if (isLocked) return;
+
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        movement = new Vector2(h, v);
+
+        if (movement.magnitude > 1) //нормализация, когда перс двигается по диагонали
+            movement = movement.normalized;
+    }
+    public void LockMovement()
+    {
+        lockCount++;
+    }
+    public void UnlockMovement()
+    {
+        lockCount = Mathf.Max(0, lockCount - 1); //уменьшаем, но не меньше 0
+    }
+    //TODO: привязать блокировку к событию.
+}
